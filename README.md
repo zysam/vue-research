8d32c97945806ca12f6aca09eae06e33eb79ee40
sysam <explnwind@gmail.com>
2017-03-06 02:31:19 +0800

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+## vue-research
+
+调研 vue 框架
+
+## Demo
+- vue-admin-demo 基于 vue + vuex 搭建的管理后台
---
+++ vue-admin-demo/src/main.js
@@ -0,0 +1,33 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import App from './App'
+import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.use(VueRouter)
+Vue.use(VueResource)
+
+Vue.config.productionTip = false
+
+const routes = [{
+  path: '/',
+  component: Home
+}, {
+  path: '/home',
+  component: Home
+}]
+
+const router = new VueRouter({
+  routes
+})
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  ...App
+})
